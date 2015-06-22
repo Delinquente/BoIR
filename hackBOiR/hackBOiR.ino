607b7655cb3e6c32b1a4883c9ae25c9c1320de00
Rathien <pturowicz@gmail.com>
2015-06-22 13:35:11 +0100

    Update hackBOiR.ino
    Clean up

--- hackBOiR/hackBOiR.ino
+++ hackBOiR/hackBOiR.ino
@@ -9,23 +9,19 @@ void setup() {
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  //lcd.print("hello, world!");
   Serial.begin(9600);
   Serial.print("Program Initiated\n"); 
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
       while (Serial.available()>0){   
         character=Serial.read();
         msg.concat(character);
        delay(10);   
     }
     
-  //lcd.setCursor(0, 0);
-  Serial.println(msg);
-  // print the number of seconds since reset:
+  lcd.setCursor(0, 0);
+  //Serial.println(msg);
   for (int i = 0; i < 16; i++){
     lcd.setCursor(i, 0);
     lcd.print(msg[i]);
